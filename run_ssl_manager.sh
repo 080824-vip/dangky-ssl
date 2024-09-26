ac9dd51adb92fde23da96cf1353dd257ee4ac88d
Your Name <you@example.com>
2024-09-26 05:47:45 +0000

    Add script to run SSL manager from GitHub

---
+++ run_ssl_manager.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# Tải về script Python từ GitHub
+curl -O https://raw.githubusercontent.com/080824-vip/dangky-ssl/main/ssl_manager.py
+
+# Chạy script Python
+python3 ssl_manager.py
